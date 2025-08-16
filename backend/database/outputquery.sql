4a6dd21f1a3e8590d8585b1e8ba2c7996ab2e144
Divesh Chauhan <165630306+divesh2022@users.noreply.github.com>
2025-08-16 07:28:30 +0530

    Create outputquery.sql

---
+++ backend/database/outputquery.sql
@@ -0,0 +1,19 @@
+USE sehat_sathi;
+
+SELECT * FROM Patient;
+SELECT * FROM NaiveUser;
+SELECT * FROM Caregiver;
+SELECT * FROM CallCenterOperator;
+SELECT * FROM Doctor;
+SELECT * FROM ASHAWorker;
+SELECT * FROM HospitalStaff;
+SELECT * FROM Admin;
+SELECT * FROM CallAssistance;
+SELECT * FROM ASHA_Assignments;
+SELECT * FROM HospitalResponse;
+SELECT * FROM AdminManagement;
+SELECT * FROM DoctorPatientAssignment;
+SELECT * FROM SMSNotification;
+SELECT * FROM Prescription;
+SELECT * FROM AuditLog;
+SELECT * FROM Feedback;
